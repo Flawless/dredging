687052fc7075d08ec560afff12a93087b1014d12
Alexander Ushanov <flawless@last-try.org>
2019-01-21 16:13:48 +0300

    Added description

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(name='dredging',
-      version='0.1',
+      version='0.1.1',
       description='Collection of helper functions for dredging math',
+      long_description='Collection of helper functions for dredging math',
       url='https://github.com/Flawless/dredging',
       author='Alexander Ushanov',
       author_email='alushanov92@gmail.com',
